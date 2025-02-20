2eb857740c7e6fd368ecab07160574a2810eca57
Azhar <128639611+syedazharmbnr1@users.noreply.github.com>
2025-02-20 11:33:53 +0300

    Update App.tsx with latest routing configuration

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { Toaster } from '@/components/ui/toaster'
-import { ThemeProvider } from '@/components/theme-provider'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Record from './pages/Record'
-import Edit from './pages/Edit'
-import Auth from './pages/Auth'
-import { SupabaseProvider } from './contexts/SupabaseContext'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { Toaster } from "@/components/ui/toaster"
+import { ThemeProvider } from "@/components/theme-provider"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Record from "./pages/Record"
+import Edit from "./pages/Edit"
+import Auth from "./pages/Auth"
+import { SupabaseProvider } from "./contexts/SupabaseContext"
 
 function App() {
   return (
