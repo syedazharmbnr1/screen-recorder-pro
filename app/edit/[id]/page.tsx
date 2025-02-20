f26e8537d911b960160ab19d1472b524e89805cb
Azhar <128639611+syedazharmbnr1@users.noreply.github.com>
2025-02-20 12:03:05 +0300

    Add dynamic edit and export pages

--- app/edit/[id]/page.tsx
+++ app/edit/[id]/page.tsx
@@ -7,8 +7,11 @@ interface EditPageProps {
 export default function EditPage({ params }: EditPageProps) {
   return (
     <div className="container mx-auto p-4">
-      <h1>Edit Recording: {params.id}</h1>
+      <h1 className="text-2xl font-bold mb-4">Edit Recording: {params.id}</h1>
       {/* TODO: Add video editing interface */}
+      <div className="bg-gray-100 p-6 rounded-lg">
+        <p>Video editing functionality will be implemented here.</p>
+      </div>
     </div>
   )
 }
--- app/export/[id]/page.tsx
+++ app/export/[id]/page.tsx
@@ -7,8 +7,11 @@ interface ExportPageProps {
 export default function ExportPage({ params }: ExportPageProps) {
   return (
     <div className="container mx-auto p-4">
-      <h1>Export Recording: {params.id}</h1>
-      {/* TODO: Add export options and processing */}
+      <h1 className="text-2xl font-bold mb-4">Export Recording: {params.id}</h1>
+      {/* TODO: Add export options */}
+      <div className="bg-gray-100 p-6 rounded-lg">
+        <p>Video export options and processing will be implemented here.</p>
+      </div>
     </div>
   )
 }
